b593aa524b0f3ee80990ed6a5d4f709327ad1007
songjeonghyun11 <thdwjd7899@gmail.com>
2020-09-09 17:04:46 +0900

    git commit

---
+++ dacon/coing.py
@@ -0,0 +1,17 @@
+# 문자열 자르기
+
+a = "naver daum"
+
+print(a.split(' ')) 
+print(a.split(' ')[0])
+
+
+# 문자열 합치기
+daum = "Daum"
+kakao = "KAKAO"
+
+print(daum + kakao)
+print(daum + '' + kakao)
+
+
+
--- dacon/model.py
+++ dacon/model.py
@@ -1,54 +1,183 @@
-import numpy
-import keras
-from keras.datasets import mnist
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Convolution2D, MaxPooling2D
-from keras import backend as K
+# Import Module
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-img_rows = 28
-img_cols = 28
+train = pd.read_csv('./data/train.csv')
+test = pd.read_csv('./data/test.csv')
 
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+train_img = train.iloc[:,3:].to_numpy().reshape(-1,28,28,1)
+train_digit = train['digit']
+train_letter = train['letter']
+test_img = test.iloc[:,2:].to_numpy().reshape(-1,28,28,1)
+test_letter = test['letter']
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+train_img_norm = train_img / 255.0
+test_img_norm = test_img / 255.0
 
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
+features = train_img_norm
+labels = train_digit.to_numpy()
 
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
+X_test = test_img_norm
 
-model=Sequential()
-model.add(Convolution2D(filters=32, kernel_size=(3,3), padding='same', activation='relu', input_shape=(28,28,1)))
-model.add(MaxPooling2D(pool_size=(2,2)))
+from sklearn.model_selection import train_test_split
 
-model.add(Convolution2D(filters=64, kernel_size=(2,2), padding='same',activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
+validation_split = 0.2
 
-model.add(Flatten())
-model.add(Dense(units=3136, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(units=10, activation='softmax'))
+X_train, X_val, y_train, y_val = train_test_split(features, labels, test_size = validation_split, random_state = 1004)
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = model.fit(x=x_train,
-                    y=y_train,
-                    batch_size=100,
-                    epochs=15,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
+def createFolder(directory):
+    
+    import os
+    
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except:
+        print("Error: Falied creating directory" + directory)
 
+def plot_curve(epochs, hist, list_of_metrics):
+    
+    fig, ax = plt.subplots(1,2,figsize = (12, 8))
+    
+    for i in range(len(ax)):
+        ax[i].set_xlabel('Epochs')
+        ax[i].set_ylabel('Value')
+        
+        for n in range(len(list_of_metrics)):
+            if i == 0:
+                y = hist[list_of_metrics[n]]
+                ax[i].plot(epochs, y)
+                ax[i].set_title('Loss')
+                if n == 1:
+                    break
+            else:
+                if n >= 2:
+                    y = hist[list_of_metrics[n]]
+                    ax[i].plot(epochs, y)
+                    ax[i].set_title('Accuracy')
+                    
+    plt.show()
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+def create_cnn(learning_rate):
+    
+    # Remove the previous model.
+    model = None
+    
+    # Input layer
+    img_input = layers.Input(shape = (28,28,1))
+    
+    # CNN
+    # Identity mapping shortcut을 위한 conv_1 layer
+    conv_1 = layers.Conv2D(128, kernel_size = 3, padding = 'same', activation = 'relu')(img_input) 
+    
+    conv_2_1 = layers.Conv2D(128, kernel_size = 3, padding = 'same', activation = 'relu')(conv_1)
+    conv_2_1 = layers.Conv2D(128, kernel_size = 3, padding = 'same')(conv_2_1)
+    
+    # ShortCut connection
+    add_2_1 = layers.add([conv_1, conv_2_1])
+    out_2_1 = layers.Activation('relu')(add_2_1)
+    
+    conv_2_2 = layers.Conv2D(128, kernel_size = 3, padding = 'same', activation = 'relu')(out_2_1)
+    conv_2_2 = layers.Conv2D(128, kernel_size = 3, padding = 'same')(conv_2_2)
+    
+    # ShortCut connection
+    add_2_2 = layers.add([out_2_1, conv_2_2])
+    out_2_2 = layers.Activation('relu')(add_2_1)
+    
+    pool_2 = layers.MaxPool2D((2,2), strides = 2)(out_2_2)
+    
+    conv_3_0 = layers.Conv2D(256, kernel_size = 1, strides = 1)(pool_2)
+    
+    conv_3_1 = layers.Conv2D(256, kernel_size = 3, padding = 'same', activation = 'relu')(conv_3_0)
+    conv_3_1 = layers.Conv2D(256, kernel_size = 3, padding = 'same')(conv_3_1)
+    
+    # ShortCut connection
+    add_3_1 = layers.add([conv_3_0, conv_3_1])
+    out_3_1 = layers.Activation('relu')(add_3_1)
+    
+    conv_3_2 = layers.Conv2D(256, kernel_size = 3, padding = 'same', activation = 'relu')(out_3_1)
+    conv_3_2 = layers.Conv2D(256, kernel_size = 3, padding = 'same')(conv_3_2)
+    
+    # ShortCut connection
+    add_3_2 = layers.add([out_3_1, conv_3_2])
+    out_3_2 = layers.Activation('relu')(add_3_2)
+    
+    pool_3 = layers.MaxPool2D((2,2), strides = 2)(out_3_2)
+    
+    conv_4_0 = layers.Conv2D(256, kernel_size = 1, strides = 1)(pool_3)
+    
+    conv_4_1 = layers.Conv2D(256, kernel_size = 3, padding = 'same', activation = 'relu')(conv_4_0)
+    conv_4_1 = layers.Conv2D(256, kernel_size = 3, padding = 'same')(conv_4_1)
+    
+    # ShortCut connection
+    add_4_1 = layers.add([conv_4_0, conv_4_1])
+    out_4_1 = layers.Activation('relu')(add_4_1)
+    
+    pool_4 = layers.MaxPool2D((2,2), strides = 2)(out_4_1)
+    
+    # FC layers
+    img_features = layers.Flatten()(pool_4)
+    img_features = layers.Dense(512, activation = 'relu')(img_features)
+    img_features = layers.Dropout(rate = 0.5)(img_features)
+    img_features = layers.Dense(512, activation = 'relu')(img_features)
+    img_features = layers.Dropout(rate = 0.5)(img_features)
+    
+    # Output layer
+    digit_pred = layers.Dense(10, activation = 'softmax')(img_features)
+    
+    model = keras.Model(inputs = img_input, outputs = digit_pred)
+    
+    model.compile(optimizer = tf.keras.optimizers.Adam(lr = learning_rate),
+                 loss = 'sparse_categorical_crossentropy',
+                 metrics = ['accuracy'])
+                    
+    return model
+
+def train_model_v1(model, X_train, y_train, X_val, y_val, epochs, batch_size = None, validation_split = 0.1):
+    
+    callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_accuracy', patience = 10, restore_best_weights = True)
+    
+    history = model.fit(x = X_train, y = y_train, epochs = epochs, batch_size = batch_size,
+                       shuffle = True, validation_data = [X_val, y_val], callbacks = [callback])
+    
+    epochs = history.epoch
+    hist = pd.DataFrame(history.history)
+    
+    return epochs, hist
+
+def train_model_v2(model, X_train, y_train, X_val, y_val, epochs, batch_size = None, validation_split = 0.1):
+    
+    callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 10, restore_best_weights = True)
+    
+    history = model.fit(x = X_train, y = y_train, epochs = epochs, batch_size = batch_size,
+                       shuffle = True, validation_data = [X_val, y_val], callbacks = [callback])
+    
+    epochs = history.epoch
+    hist = pd.DataFrame(history.history)
+    
+    return epochs, hist
+
+model_list = []
+
+for i in range(10):
+    model = create_cnn(learning_rate = 0.0001)
+    model_list.append(model)
+
+### Training
+epochs = 200
+batch_size = 1
+validation_split = 0.2
+
+for i in range(len(model_list)):
+    print("***************Trainig_my_model_{}*****************".format(i))
+    epoch, hist = train_model_v1(model_list[i], X_train, y_train, X_val, y_val, epochs, batch_size) 
+
+    # Plot a graph of the metric vs. epochs.
+    list_of_metrics_to_plot = ['loss','val_loss','accuracy','val_accuracy']
+    plot_curve(epoch, hist, list_of_metrics_to_plot)
