e344770867accc83aec85108750f5b1d062d049b
songjeonghyun11 <thdwjd7899@gmail.com>
2020-09-14 01:24:26 +0900

    git commit

--- face_study/1.py
+++ face_study/1.py
@@ -1,46 +1,303 @@
-from random import choice
-from numpy import load
-from numpy import expand_dims
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import Normalizer
-from sklearn.svm import SVC
-from matplotlib import pyplot
-# 얼굴 불러오기
-data = load('5-celebrity-faces-dataset.npz')
-testX_faces = data['arr_2']
-# 얼굴 임베딩 불러오기
-data = load('5-celebrity-faces-embeddings.npz')
-trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
-# 입력 벡터 일반화
-in_encoder = Normalizer(norm='l0')
-trainX = in_encoder.transform(trainX)
-testX = in_encoder.transform(testX)
-# 목표 레이블 암호화
-out_encoder = LabelEncoder()
-out_encoder.fit(trainy)
-trainy = out_encoder.transform(trainy)
-testy = out_encoder.transform(testy)
-# 모델 적합
-model = SVC(kernel='linear', probability=True)
-model.fit(trainX, trainy)
-# 테스트 데이터셋에서 임의의 예제에 대한 테스트 모델
-selection = choice([i for i in range(testX.shape[0])])
-random_face_pixels = testX_faces[selection]
-random_face_emb = testX[selection]
-random_face_class = testy[selection]
-random_face_name = out_encoder.inverse_transform([random_face_class])
-# 얼굴 예측
-samples = expand_dims(random_face_emb, axis=0)
-yhat_class = model.predict(samples)
-yhat_prob = model.predict_proba(samples)
-# 이름 얻기
-class_index = yhat_class[0]
-class_probability = yhat_prob[0,class_index] * 100
-predict_names = out_encoder.inverse_transform(yhat_class)
-print('예상: %s (%.3f)' % (predict_names[0], class_probability))
-print('추측: %s' % random_face_name[0])
-# 재미삼아 그리기
-pyplot.imshow(random_face_pixels)
-title = '%s (%.3f)' % (predict_names[0], class_probability)
-pyplot.title(title)
-pyplot.show()
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Base libraries
+import os
+import random
+
+# Keras libraries
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate, BatchNormalization, UpSampling2D
+from keras.layers import  Dropout, Activation
+from keras.optimizers import Adam, SGD
+from keras.layers.advanced_activations import LeakyReLU
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras import backend as K
+from keras.utils import plot_model
+
+print(tf.__version__)
+
+def save_model(model_name: str, model: keras.models.Model):
+    model_json = model.to_json()
+    with open(model_name, "w") as json_file:
+        json_file.write(model_json)
+
+from keras.models import model_from_json
+from typing import Optional
+
+def load_model(
+    model_name: str, with_weights_path: Optional[str] = None
+) -> keras.models.Model:
+    json_file = open(model_name, "r")
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    if with_weights_path:
+        model.load_weights(with_weights_path)
+    return model
+
+# 구글 드라이브 import
+from google.colab import drive
+drive.mount('/gdrive', force_remount=True)
+
+import os
+gdrive_path = os.path.join("/gdrive", "My Drive", "lecture", "resources")
+
+import cv2
+import os
+
+#다양한 크기
+
+mask = cv2.imread(os.path.join("annotations", "trimaps", "Abyssinian_2.png"))
+print(mask.shape)
+
+img = cv2.imread(os.path.join("images", "Abyssinian_2.jpg"))
+print(img.shape)
+
+resized_img = cv2.resize(img, (256, 256))
+print(resized_img.shape)
+
+# 이미지와 마스크가 서로 다른 파일 개수
+
+print(len(os.listdir(os.path.join("annotations", "trimaps"))))
+print(len(os.listdir(os.path.join("images"))))
+
+# 이미지는 jpg, 마스크는 png.
+
+len(list(filter(lambda el: el[-3:] == "png", os.listdir(os.path.join("annotations", "trimaps")))))
+
+
+# 이미지는 color. 마스크는 grayscale.
+
+batch_size = 32
+seed = 42
+
+image_files = os.listdir("images")
+image_names = list(map(lambda v: v[:-4], image_files))
+
+random.seed(seed)
+random.shuffle(image_names)
+
+split = int(0.95 * len(image_names))
+train_image_names = image_names[0:split]
+test_image_names  = image_names[split:]
+
+val_split = int(0.9 * len(train_image_names))
+val_image_names = train_image_names[val_split:]
+train_image_names = train_image_names[0:val_split]
+
+import toolz
+import numpy as np
+
+def img_mask_generator(img_path, mask_path, image_files, mask_files, batch_size=1, target_size=(256, 256)):
+  total_file_num = len(image_files)
+  index = -1
+  while True:
+    index = 0 if index > ((total_file_num // batch_size) - 2) else index+1
+    img_batch = []
+    mask_batch = []
+    
+    for i in range(batch_size):
+      img = cv2.imread(os.path.join(img_path, image_files[batch_size*index+i]), cv2.IMREAD_COLOR)
+      img = cv2.resize(img, target_size, interpolation=cv2.INTER_NEAREST)
+
+      img = img / 255.
+
+      img_batch.append(img)
+      
+      mask = cv2.imread(os.path.join(mask_path, mask_files[batch_size*index+i]), cv2.IMREAD_GRAYSCALE)
+      mask = cv2.resize(mask, target_size, interpolation=cv2.INTER_NEAREST)
+
+      mask[mask >= 2] = 0
+      mask[mask != 0] = 1
+      
+      mask = np.reshape(mask, (mask.shape[0], mask.shape[1], 1))
+      mask_batch.append(mask)
+
+    yield np.array(img_batch), np.array(mask_batch)
+
+with_ext = lambda v, ext: "{}.{}".format(v, ext)
+
+train_batch_generator = img_mask_generator(
+    img_path=os.path.join("images"),
+    mask_path=os.path.join("annotations", "trimaps"),
+    image_files=list(map(lambda v: with_ext(v, "jpg"), train_image_names)),
+    mask_files=list(map(lambda v: with_ext(v, "png"), train_image_names)),
+    batch_size=batch_size,
+    target_size=(256, 256))
+
+val_batch_generator = img_mask_generator(
+    img_path=os.path.join("images"),
+    mask_path=os.path.join("annotations", "trimaps"),
+    image_files=list(map(lambda v: with_ext(v, "jpg"), val_image_names)),
+    mask_files=list(map(lambda v: with_ext(v, "png"), val_image_names)),
+    batch_size=batch_size,
+    target_size=(256, 256))
+
+test_batch_generator = img_mask_generator(
+    img_path=os.path.join("images"),
+    mask_path=os.path.join("annotations", "trimaps"),
+    image_files=list(map(lambda v: with_ext(v, "jpg"), test_image_names)),
+    mask_files=list(map(lambda v: with_ext(v, "png"), test_image_names)),
+    batch_size=batch_size,
+    target_size=(256, 256))
+
+train_batch_generator = img_mask_generator(
+    img_path=os.path.join("images"),
+    mask_path=os.path.join("annotations", "trimaps"),
+    image_files=list(map(lambda v: with_ext(v, "jpg"), train_image_names)),
+    mask_files=list(map(lambda v: with_ext(v, "png"), train_image_names)),
+    batch_size=batch_size,
+    target_size=(256, 256))
+
+val_batch_generator = img_mask_generator(
+    img_path=os.path.join("images"),
+    mask_path=os.path.join("annotations", "trimaps"),
+    image_files=list(map(lambda v: with_ext(v, "jpg"), val_image_names)),
+    mask_files=list(map(lambda v: with_ext(v, "png"), val_image_names)),
+    batch_size=batch_size,
+    target_size=(256, 256))
+
+test_batch_generator = img_mask_generator(
+    img_path=os.path.join("images"),
+    mask_path=os.path.join("annotations", "trimaps"),
+    image_files=list(map(lambda v: with_ext(v, "jpg"), test_image_names)),
+    mask_files=list(map(lambda v: with_ext(v, "png"), test_image_names)),
+    batch_size=batch_size,
+    target_size=(256, 256))
+
+def mean_iou(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
+    inter = tf.math.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
+    union = tf.math.count_nonzero(tf.add(yt0, yp0))
+    iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
+    return iou
+
+def unet(sz = (256, 256, 3)):
+    x = Input(sz)
+    inputs = x
+    
+    # down sampling 
+    # f = 8
+    layers = []
+    
+    for i in range(0, 6):
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        layers.append(x)
+        x = MaxPooling2D() (x)
+        f = f*2
+    ff2 = 64 
+  
+    # bottleneck 
+    j = len(layers) - 1
+    x = Conv2D(f, 3, activation='relu', padding='same') (x)
+    x = Conv2D(f, 3, activation='relu', padding='same') (x)
+    x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same') (x)
+    x = Concatenate(axis=3)([x, layers[j]])
+    j = j -1 
+  
+  # upsampling 
+    for i in range(0, 5):
+        ff2 = ff2//2
+        f = f // 2 
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        x = Conv2D(f, 3, activation='relu', padding='same') (x)
+        x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same') (x)
+        x = Concatenate(axis=3)([x, layers[j]])
+        j = j -1 
+    
+  
+  # classification 
+    x = Conv2D(f, 3, activation='relu', padding='same') (x)
+    x = Conv2D(f, 3, activation='relu', padding='same') (x)
+    outputs = Conv2D(1, 1, activation='sigmoid') (x)
+  
+  # model creation 
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = [mean_iou])
+  
+    return model
+
+model = unet()
+model.summary()
+
+
+def build_callbacks():
+        checkpointer = ModelCheckpoint(filepath='unet.h5', verbose=0, save_best_only=True, save_weights_only=True)
+        callbacks = [checkpointer, PlotLearning()]
+        return callbacks
+
+val_image_list = list(map(lambda v: with_ext(v, "jpg"), val_image_names))
+
+# inheritance for training process plot 
+class PlotLearning(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.i = 0
+        self.x = []
+        self.losses = []
+        self.val_losses = []
+        self.acc = []
+        self.val_acc = []
+        #self.fig = plt.figure()
+        self.logs = []
+        
+    def on_epoch_end(self, epoch, logs={}):
+        self.logs.append(logs)
+        self.x.append(self.i)
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.acc.append(logs.get('mean_iou'))
+        self.val_acc.append(logs.get('val_mean_iou'))
+        self.i += 1
+        # print('i=',self.i,'loss=',logs.get('loss'),'val_loss=',logs.get('val_loss'),'mean_iou=',logs.get('mean_iou'),'val_mean_iou=',logs.get('val_mean_iou'))
+        
+        # choose a random test image and preprocess
+        path = np.random.choice(val_image_list)
+        img = cv2.imread(f'images/{path}')
+        img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_NEAREST)
+        img = img / 255.
+
+        target_mask = cv2.imread(os.path.join("annotations", "trimaps", "{}.png".format(path[:-4])))
+        target_mask = cv2.resize(target_mask, (256, 256), interpolation=cv2.INTER_NEAREST)
+        target_mask[target_mask >= 2] = 0
+        target_mask[target_mask != 0] = 1
+        
+        # predict the mask 
+        pred = model.predict(np.expand_dims(img, 0))
+        
+        # mask post-processing 
+        msk = pred.squeeze()
+        msk = np.stack((msk,)*3, axis=-1)
+        msk[msk >= 0.5] = 1 
+        msk[msk < 0.5] = 0 
+        
+        # show the mask and the segmented image 
+        img = img.astype(np.float32) 
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        combined = np.concatenate([rgb_img, msk, rgb_img * msk, target_mask], axis = 1)
+        plt.axis('off')
+        plt.imshow(combined)
+        plt.show()
+
+train_steps = len(train_image_names) // batch_size
+val_steps = len(val_image_names) // batch_size
+
+history = model.fit(
+    train_batch_generator,
+    epochs = 10,
+    steps_per_epoch = train_steps,
+    validation_data = val_batch_generator, 
+    validation_steps = val_steps,
+    callbacks = build_callbacks(), 
+    verbose = 1)
+
+test_steps = len(test_image_names) // batch_size
+test_loss, test_acc = model.evaluate(test_batch_generator, steps=test_steps, verbose=2)
+print('테스트 정확도:', test_acc)
